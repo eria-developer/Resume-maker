563abd3c9ab5f4deaa4e87ed6e1d767a2024d962
eria-developer <eriddeveloper@gmail.com>
2024-06-23 19:47:23 +0300

    added sample views

--- backend/resume/views.py
+++ backend/resume/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Profile, Education, Experience, Skill, Leadership, Reference, Certification, Project, Language
+from .serializers import (ProfileSerializer, EducationSerializer, ExperienceSerializer, SkillSerializer,
+                          LeadershipSerializer, ReferenceSerializer, CertificationSerializer, ProjectSerializer, LanguageSerializer)
 
-# Create your views here.
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+class EducationViewSet(viewsets.ModelViewSet):
+    queryset = Education.objects.all()
+    serializer_class = EducationSerializer
+
+class ExperienceViewSet(viewsets.ModelViewSet):
+    queryset = Experience.objects.all()
+    serializer_class = ExperienceSerializer
+
+class SkillViewSet(viewsets.ModelViewSet):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
+
+class LeadershipViewSet(viewsets.ModelViewSet):
+    queryset = Leadership.objects.all()
+    serializer_class = LeadershipSerializer
+
+class ReferenceViewSet(viewsets.ModelViewSet):
+    queryset = Reference.objects.all()
+    serializer_class = ReferenceSerializer
+
+class CertificationViewSet(viewsets.ModelViewSet):
+    queryset = Certification.objects.all()
+    serializer_class = CertificationSerializer
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class LanguageViewSet(viewsets.ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
